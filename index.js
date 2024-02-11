3173a317ef9151e0a7e82d7aa0276ac3ac1173b9
A.V.S.Chaitanya <chaithanya1996@outlook.com>
2024-02-11 11:41:12 +0530

    add login

--- index.js
+++ index.js
@@ -2,3 +2,28 @@ function square(number) {
     alert(number*number);
 }
   
+function login() {
+    let oauthEndpoint = "https://keycloak.app.adapa.me/realms/calorie-tracker"
+    let loginForm = document.createElement("form")
+    loginForm.setAttribute("method","GET")
+    loginForm.setAttribute("action","oauthEndpoint")
+
+    let oauthParameters = {
+        "client_id" : "calorie-tracker-app",
+        "redirect_uri" : "",
+        "state" : "pass-through-value",
+        "response_type" : "token",
+        "scope" : "calorie-user"
+    }
+
+    for (var p in oauthParameters) {
+        let input = document.createElement("input")
+        input.setAttribute("type","hidden")
+        input.setAttribute("value",oauthParameters[p])
+        input.setAttribute("name",p)
+        loginForm.appendChild(input)
+    }
+    document.body.appendChild(loginForm)
+
+    loginForm.submit()
+}
