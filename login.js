a39bff2d12eec0402616016e64b299f99d304559
A.V.S.Chaitanya <chaithanya1996@outlook.com>
2024-03-08 04:52:05 +0530

    dd

--- login.js
+++ login.js
@@ -1,4 +1,5 @@
-// set the dimensions and margins of the graph
+function chartPlot() {
+    // set the dimensions and margins of the graph
 const margin = {top: 10, right: 30, bottom: 30, left: 40},
 width = 460 - margin.left - margin.right,
 height = 400 - margin.top - margin.bottom;
@@ -50,3 +51,4 @@ svg.selectAll("rect")
     .style("fill", "#69b3a2")
 
 });
+}
