779814afd22569ac5a325d5ed4aaea1a291f62d4
Adapa, Venkata Sai Chaitanya (Chaitanya) <venkatasaichaitanya.adapa@ge.com>
2024-03-13 02:43:21 +0530

    psuh node module

--- authProcess.js
+++ authProcess.js
@@ -24,5 +24,66 @@ function getQueryVariable(variable) {
   alert('Query Variable ' + variable + ' not found');
 }
 
-var param1var = getQueryVariable("code");
-document.cookie = `code=${param1var};`
+
+const client = new OAuth2Client({
+
+    // The base URI of your OAuth2 server
+    server: 'https://keycloak.app.adapa.me/realms/calorie-tracker/',
+  
+    // OAuth2 client id
+    clientId: 'calorie-tracker-app',
+  
+    // OAuth2 client secret. Only required for 'client_credentials', 'password'
+    // flows. Don't specify this in insecure contexts, such as a browser using
+    // the authorization_code flow.
+    clientSecret: '...',
+  
+  
+    // The following URIs are all optional. If they are not specified, we will
+    // attempt to discover them using the oauth2 discovery document.
+    // If your server doesn't have support this, you may need to specify these.
+    // you may use relative URIs for any of these.
+  
+  
+    // Token endpoint. Most flows need this.
+    // If not specified we'll use the information for the discovery document
+    // first, and otherwise default to /token
+    tokenEndpoint: '/token',
+  
+    // Authorization endpoint.
+    //
+    // You only need this to generate URLs for authorization_code flows.
+    // If not specified we'll use the information for the discovery document
+    // first, and otherwise default to /authorize
+    authorizationEndpoint: '/auth',
+  
+    // OAuth2 Metadata discovery endpoint.
+    //
+    // This document is used to determine various server features.
+    // If not specified, we assume it's on /.well-known/oauth2-authorization-server
+    discoveryEndpoint: '/.well-known/oauth2-authorization-server',
+  
+  });
+
+  const codeVerifier = await generateCodeVerifier();
+  const oauth2Token = await client.authorizationCode.getTokenFromCodeRedirect(
+    document.location,
+    {
+      /**
+       * The redirect URI is not actually used for any redirects, but MUST be the
+       * same as what you passed earlier to "authorizationCode"
+       */
+      redirectUri: 'https://calorie-tracker.pages.dev/callback',
+  
+      /**
+       * This is optional, but if it's passed then it also MUST be the same as
+       * what you passed in the first step.
+       *
+       * If set, it will verify that the server sent the exact same state back.
+       */
+      state: 'some-string',
+  
+      codeVerifier,
+  
+    }
+  );
--- index.js
+++ index.js
@@ -27,3 +27,62 @@ function login() {
 
     loginForm.submit()
 }
+
+const client = new OAuth2Client({
+
+    // The base URI of your OAuth2 server
+    server: 'https://keycloak.app.adapa.me/realms/calorie-tracker/',
+  
+    // OAuth2 client id
+    clientId: 'calorie-tracker-app',
+  
+    // OAuth2 client secret. Only required for 'client_credentials', 'password'
+    // flows. Don't specify this in insecure contexts, such as a browser using
+    // the authorization_code flow.
+    ///clientSecret: '...',
+  
+  
+    // The following URIs are all optional. If they are not specified, we will
+    // attempt to discover them using the oauth2 discovery document.
+    // If your server doesn't have support this, you may need to specify these.
+    // you may use relative URIs for any of these.
+  
+  
+    // Token endpoint. Most flows need this.
+    // If not specified we'll use the information for the discovery document
+    // first, and otherwise default to /token
+    tokenEndpoint: '/token',
+  
+    // Authorization endpoint.
+    //
+    // You only need this to generate URLs for authorization_code flows.
+    // If not specified we'll use the information for the discovery document
+    // first, and otherwise default to /authorize
+    authorizationEndpoint: '/auth',
+  
+    // OAuth2 Metadata discovery endpoint.
+    //
+    // This document is used to determine various server features.
+    // If not specified, we assume it's on /.well-known/oauth2-authorization-server
+    discoveryEndpoint: '/.well-known/oauth2-authorization-server',
+  
+  });
+
+
+  const codeVerifier = await generateCodeVerifier();
+
+// In a browser this might work as follows:
+document.location = await client.authorizationCode.getAuthorizeUri({
+
+  // URL in the app that the user should get redirected to after authenticating
+  redirectUri: 'https://calorie-tracker.pages.dev/callback',
+
+  // Optional string that can be sent along to the auth server. This value will
+  // be sent along with the redirect back to the app verbatim.
+  state: 'some-string',
+
+  codeVerifier,
+
+  scope: ['calorie-user']
+
+});
